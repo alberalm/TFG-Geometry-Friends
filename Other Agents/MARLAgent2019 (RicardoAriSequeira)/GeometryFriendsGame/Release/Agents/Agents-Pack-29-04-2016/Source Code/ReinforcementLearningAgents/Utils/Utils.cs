e9bef5866c2d124c651e7dc44b91888779e41694
Juan Carlos Llamas Núñez <jcllanu@gmail.com>
2023-07-18 11:17:12 +0200

    Delete Other Agents/MARLAgent2019 (RicardoAriSequeira)/GeometryFriendsGame/Release/Agents/Agents-Pack-29-04-2016 directory

--- Other Agents/MARLAgent2019 (RicardoAriSequeira)/GeometryFriendsGame/Release/Agents/Agents-Pack-29-04-2016/Source Code/ReinforcementLearningAgents/Utils/Utils.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GeometryFriendsAgents.Utils {
-    class Utils {
-        public static double CalculateL2(double xPos, double yPos, double p1, double p2) {
-            return Math.Abs(Math.Pow(p1 - xPos, 2) + Math.Pow(p2 - yPos, 2));
-        }
-    }
-}
